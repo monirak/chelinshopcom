e5d8509d4653bbb3b2f2629d1cf9648692859e86
yim monirak <52792657+monirak@users.noreply.github.com>
2023-06-20 13:31:34 +0700

    Update server.js

--- seatv_rtmp01/auth/server.js
+++ seatv_rtmp01/auth/server.js
@@ -8,7 +8,7 @@ app.post("/auth", function (req, res) {
   const streamkey = req.body.key;
 
   /* You can make a database of users instead :) */
-  if (streamkey === "ppnews") {
+  if (streamkey === "WWB99") {
     res.status(200).send();
     return;
   }
